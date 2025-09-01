30b6b5e3121aec1d3a48632fa1a1d45bc8a9bbcf
Peter Krusche <pkrusche@gmail.com>
2025-09-01 16:06:34 +0200

    fix plot collection and add test

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -4,16 +4,14 @@ tryCatch(
   },
   error = function(e) {
     # Try to load all source files manually if replr package is not installed
-    tryCatch({
-      source("R/replr-package.R")
-      source("R/debug.R")
-      source("R/communication.R")
-      source("R/utils.R") 
-      source("R/session.R")
-      source("R/ellmer-tools.R")
-    }, error = function(e2) {
-      # Skip loading if we can't load source files
-      cat("Warning: Could not load replr package or source files:", e2$message, "\n")
-    })
+    tryCatch(
+      {
+        devtools::load_all()
+      },
+      error = function(e2) {
+        # Skip loading if we can't load source files
+        cat("Warning: Could not load replr package or source files:", e2$message, "\n")
+      }
+    )
   }
 )
